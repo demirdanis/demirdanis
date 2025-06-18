39612edf8a5f82e1fb96fa7dcbfc214f34096cec
demirdanis <demirdanis@gmail.com>
2025-06-18 21:45:00 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 const isGithubPages = process.env.DEPLOY_TARGET === "gh-pages";
-const repoName = "repo-adin"; // GitHub repo adını buraya yaz
-
+const repoName = "demirdanis";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isGithubPages ? `/${repoName}` : "",
