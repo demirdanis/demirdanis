f0934a2801a905be353bb02a93d393f1e61b5bbe
demirdanis <demirdanis@gmail.com>
2025-06-18 22:02:01 +0300

    Updates

--- src/components/navigation/navigation.types.tsx
+++
@@ -1,9 +0,0 @@
-import { PortfolioData } from "../../types/portfolio-data.types";
-
-export interface NavigationProps {
-  data: PortfolioData;
-  activeSection: string;
-  setActiveSection: (section: string) => void;
-  isMenuOpen: boolean;
-  setIsMenuOpen: (open: boolean) => void;
-}
--- src/components/services-section/services-section.types.tsx
+++
@@ -1,9 +0,0 @@
-export interface Service {
-  icon: "users" | "code" | "target" | "briefcase";
-  title: string;
-  description: string;
-}
-
-export interface ServicesSectionProps {
-  services: Service[];
-}
